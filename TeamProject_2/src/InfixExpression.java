6207c387074161f58b72908bde19d4ee94b8ca52
mhays705 <mhays705@gmail.com>
2024-02-24 14:22:06 -0600

    Fixed the parser
    Fixed the parser so it correctly handles comparison operators and logical operators.

--- TeamProject_2/src/InfixExpression.java
+++ TeamProject_2/src/InfixExpression.java
@@ -12,30 +12,54 @@ public class InfixExpression {
 	public InfixExpression(String file) throws IOException {
 		infix = new Stack<>();
 		try {
-		FileReader reader = new FileReader(file);
-		StringBuilder number = new StringBuilder(); // Store digits of number
-		int currentChar;
-		while ((currentChar = reader.read()) != -1) { // Make sure stream isn't empty
-			char ch = (char) currentChar;
-			if (Character.isDigit(ch)) { // If character is digit append it to builder
-				number.append(ch);     
-			} else if (!Character.isWhitespace(ch)) {
-			    if (number.length() > 0) {
-			        infix.push(number.toString()); // Push accumulated digits to stack
-			        number.setLength(0); // Reset StringBuilder
-			    }
-			    // Push the character onto the stack
-			    infix.push(String.valueOf(ch));
+			FileReader reader = new FileReader(file);
+			StringBuilder number = new StringBuilder(); // Store digits of number
+			StringBuilder operator = new StringBuilder(); // Store comparison and logical operators
+			int currentChar;
+			while ((currentChar = reader.read()) != -1) { // Make sure stream isn't empty
+				char ch = (char) currentChar;
+				if (Character.isDigit(ch)) { // If character is digit append it to builder
+					number.append(ch);
+				}
+				if (ch == '<' || ch == '>' || ch == '&' || ch == '|' || ch == '!') { // Check if character is comparison or logical operator
+					char nextChar = (char) reader.read();
+					if (nextChar == '=') {
+						operator.append(ch).append(nextChar); 
+						infix.push(operator.toString());
+						operator.setLength(0); // Reset operator string builder
+					}
+					else if ((ch == '&' && nextChar == '&') || (ch == '|' && nextChar == '|')) { // If logical operator append it to builder and push on to stack
+                        operator.append(ch).append(nextChar);
+                        infix.push(operator.toString());
+                        operator.setLength(0); // Reset operator string builder
+					}
+					else {
+						infix.push(String.valueOf(ch)); // Push single character operator
+					}
+				} else if (!Character.isWhitespace(ch)) {
+					char nextChar = (char) reader.read();
+					if (Character.isDigit(nextChar)) {   // Check if multidigit number
+						number.append(nextChar);
+					}
+					if (number.length() > 0) {
+						infix.push(number.toString()); // Push accumulated digits to stack
+						number.setLength(0); // Reset StringBuilder
+					}
+					// Push the character onto the stack
+					else {
+						infix.push(String.valueOf(ch));
+					}
+				
+				}
 			}
-		}
-		
-		// Push any remaining numbers on to the stack
-        if (number.length() > 0) {
-            infix.push(number.toString());
-        }
-        reader.close();
-		}
-		catch (FileNotFoundException e) {
+			
+
+			// Push any remaining numbers on to the stack
+			if (number.length() > 0) {
+				infix.push(number.toString());
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
 			System.out.println("Error: File not found.");
 		}
 		System.out.println(infix.toString()); // Remove before submitting. Only for testing
